a781072d43a342d92a44a87e290d1bc7bf1d5ded
chaojie <zhuzhezhe95@gmail.com>
2022-01-10 21:18:47 +0800

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by 朱哲哲 on 2022/1/10.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
